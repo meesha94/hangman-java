f943422efb73d95228b5c98b7d4dddf939bb3563
meesha94 <95078931+meesha94@users.noreply.github.com>
2022-09-29 12:43:04 +0100

    working hangman with lives added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,37 +1,91 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
-import static org.example.WordsUtils.generateHangman;
 import static org.example.WordsUtils.generateWord;
 
 public class Main {
-    public  static String generateHangman(){
-        return generateWord().replaceAll("[a-z/g]","_ ");
+    private static ArrayList<String> lettersPlayed = new ArrayList<>();
 
+    private static int lives = 4;
+    public  static String generateHangman(String targetWord){
+         String hangmanWord = targetWord.replaceAll("[a-z/g]","_");
+        return hangmanWord;
     }
 
-    public String checkGuess(char guess){
+    public static Boolean checkGuess(String guess, String targetWord){
+        boolean isCorrectGuess = false;
+        String lowerCaseGuess = guess.toLowerCase();
 
-        for(int i = 0; i < generateWord().length(); i++) {
-            if (generateWord().contains(guess)){
-                return "correct";
-            } else {
-                return "incorrect letter";
+
+        String[] targetWordArray = targetWord.split("");
+
+        for (int i = 0; i < targetWordArray.length; i++) {
+            if(!lettersPlayed.contains(lowerCaseGuess)) {
+                if (targetWordArray[i].equals(lowerCaseGuess)) {
+                    isCorrectGuess = true;
+                }
+
             }
         }
-        return "false";
+        return isCorrectGuess;
+    }
+
+    public static int getIndex(String guess, String targetWord){
+
+        for(int i =0; i < targetWord.length(); i++) {
+
+        }
+        return targetWord.indexOf(guess);
+
+    };
+
+    public static String dashToLetter(int index, String hangmanWord, String guess){
+
+
+
+            String newWord = hangmanWord.substring(0, index) + guess + hangmanWord.substring(index + 1);
+
+            return newWord;
+
+
     }
 
     public static void main(String[] args) {
+        boolean gameisPlaying = true;
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to hangman. Guess the word ...");
-        //System.out.println(generateWord());
-        System.out.println(generateHangman());
-        System.out.println("Guess a letter");
-        String guess = scanner.nextLine();
-        System.out.println(checkGuess(guess));
+        String targetWord = generateWord();
+        String hangmanWord = generateHangman(targetWord);
+        System.out.println(hangmanWord);
+
+        while(gameisPlaying && lives > 0) {
+            System.out.println("Guess a letter");
+            String guess = scanner.nextLine();
+            boolean isCorrectGuess = checkGuess(guess, targetWord);
+            System.out.println(isCorrectGuess);
+            int index = getIndex(guess, targetWord);
+            if (isCorrectGuess) {
+               hangmanWord = dashToLetter(index, hangmanWord, guess);
+                System.out.println(hangmanWord);
+                System.out.println(lettersPlayed.add(guess));
+            } else {
+                System.out.println("Sorry wrong guess");
+                lives--;
+                System.out.println(lives + " lives");
+
+
+
+            }
+
+
+        }
+        System.out.println("You lost! Game over!");
+        System.out.println("The word was " +  targetWord);
+
+
 
 
 
--- src/main/java/org/example/WordsUtils.java
+++ src/main/java/org/example/WordsUtils.java
@@ -6,23 +6,18 @@ import java.util.Random;
 public class WordsUtils {
     private static ArrayList<String> words = new ArrayList<>();
 
-    private  ArrayList<String> lettersPlayed = new ArrayList<>();
-
-
-
-
-
-
     private static final Random RANDOM = new Random();
 
     static {
         words.add("random");
         words.add("water");
         words.add("computers");
+        //words.add("room");
     }
 
     public static String generateWord(){
-        return words.get(RANDOM.nextInt(words.toArray().length));
+        String targetWord =  words.get(RANDOM.nextInt(words.toArray().length));
+        return targetWord;
     }
 
 
